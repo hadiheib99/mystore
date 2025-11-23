d1b96c233d65625d4de65f5e59c4a696fdf96f01
Abdelhadi Heib <hadiheib99@gmail.com>
2025-11-23 20:55:03 +0200

    change to match the project version

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,30 +1,21 @@
-// src/app/app.module.ts
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
-import { App } from './app';
 import { ProductList } from './components/product-list/product-list';
 import { ProductDetail } from './components/product-detail/product-detail';
 import { Cart } from './components/cart/cart';
 import { Confirmation } from './components/confirmation/confirmation';
-import { ProductService } from './services/product';
-import { CartService } from './services/cart';
-import { provideHttpClient } from '@angular/common/http';
-
 
 const routes: Routes = [
   { path: '', component: ProductList },
-  { path: 'products/:id', component: ProductDetail },
+  { path: 'product/:id', component: ProductDetail },
   { path: 'cart', component: Cart },
   { path: 'confirmation', component: Confirmation },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
-  declarations: [App, ProductList, ProductDetail, Cart, Confirmation],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes), Cart, Confirmation, ProductList, ProductDetail],
-  providers: [ProductService, CartService, provideHttpClient()],
-  bootstrap: [App],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppModule {}
+export class AppRoutingModule {}
